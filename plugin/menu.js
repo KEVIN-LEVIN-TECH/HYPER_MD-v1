0ab095e123b2bbab7d794d3d61fcfaa458e83d1f
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-12 19:53:49 +0530

    Update menu.js

--- plugin/menu.js
+++ plugin/menu.js
@@ -1,52 +1,67 @@
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "menu",
-    desc: "menu the bot",
-    category: "menu",
-    react: "📜",
+    react: '📜',
+    desc: "get cmd list",
+    category: "main",
     filename: __filename
 },
-
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: '',
+}
 
-let dec = `
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
+ }
+}
 
-╭─────────────━┈⊷
-│👾 ʙᴏᴛ ɴᴀᴍᴇ: HYPER MD
-│👨‍💻 ᴏᴡɴᴇʀ : Mr Senesh   
-│👤 ɴᴜᴍʙᴇʀ: 94784337506
-│💻 HOSTER: Kevin
-│💫 ᴘʀᴇғɪx: [Multi-Prefix]
-╰─────────────━┈⊷ 
-╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
-${menu.converter}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙵𝚄𝙽 ❯━╮
-${menu.fun}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙶𝚁𝙾𝚄𝙿 ❯━╮
-${menu.group}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 ❯━╮
-${menu.download}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙼𝙰𝙸𝙽 ❯━╮
+let madeMenu =`
+╭━━━━━━━━━━━━━━━━━━━ 
+👋 *Hello ${pushname}* 
+╰━━━━━━━━━━━━━━━━━━━ 
+╭━━━━━━━━━━━━━━━━━━━ 
+│👤Bot Owener: MrSenesh
+│🤖Bot Name: Hyper MD
+│🧬Prefix: [.]
+│🔖Versions: 1.0.0
+│🔮Whatsap Number: +94784337506
+╰━━━━━━━━━━━━━━━━━━━ 
+╭━━❮ MAIN COMMAND ❯━●●►
 ${menu.main}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙰𝙽𝙸𝙼𝙴 ❯━╮
-${menu.anime}
-╰━━━━━━━━━━━━━━━⪼
-╭━❮ 𝙾𝚆𝙽𝙴𝚁 ❯━╮
+╰━━━━━━━━━━━━━━━━━━━
+╭━━❮ DOWNLOAD COMMAND ❯━●●►
+${menu.download}
+╰━━━━━━━━━━━━━━━━━━━
+╭━━❮ GROUP COMMAND ❯━●●►
+${menu.group}
+╰━━━━━━━━━━━━━━━━━━━
+╭━━❮ OWNER COMMAND ❯━●●►
 ${menu.owner}
-╰━━━━━━━━━━━━━━━⪼
- ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
-`
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg`},caption:dec},{quoted:mek});
+╰━━━━━━━━━━━━━━━━━━━
+╭━━❮ CONVERT COMMAND ❯━●●►
+${menu.convert}
+╰━━━━━━━━━━━━━━━━━━━
+╭━━❮ SEARCH COMMAND ❯━●●►
+${menu.search}
+╰━━━━━━━━━━━━━━━━━━━
 
+ *POWERD BY Mr Senesh*
+`
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+    
 }catch(e){
-console.log(e)
+console.log.(e);
 reply(`${e}`)
 }
 })
