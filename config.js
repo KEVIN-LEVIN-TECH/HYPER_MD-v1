10afebdf6a5c58997ca1a0f431d80845aa96ec30
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-12 20:00:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || 'sVRl2aTB#Glpe7siUwqHUsOnIjt2CXfXf_QZgTFyAD5H_OXLFOXA',
 MONGODB: process.env.MONGODB || "mongodb://mongo:aLblyZcScobdesSHwCRxvtoIBewhIbDP@junction.proxy.rlwy.net:51487",
 };
