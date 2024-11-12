50907557e5761cf93b0731649894d87ce8e62f1f
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-12 19:55:47 +0530

    Update alive.js

--- plugin/alive.js
+++ plugin/alive.js
@@ -1,33 +1,17 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
+    react: "👋",
     desc: "Check bot online or no.",
     category: "main",
-    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-
-let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
-
-*Im Hyper-MD Whatsapp Bot Create By Mr Senesh 🍂✨*
-
-| *Version*: 1.0.0
-| *Memory*: 38.09MB/7930MB
-| *Runtime*: 1 minute,25 seconds
-| *Owner*: Nethmika
-
-I am hyper md whatsapp bot. How can I help you.
-To get the menu, type as menu. If you need to know something about the bot,
-type as owner and direct the question to me. Good day.
-
-*Mr Senesh*
-
- ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ`
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
+const config = await readEnv();
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
