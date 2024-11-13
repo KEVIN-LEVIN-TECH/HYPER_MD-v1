7ad5c04a07c047306d4fc67edf0d20194540ce56
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-13 21:31:11 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -15,16 +15,16 @@ let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 
 
 Im Hyper-MD Whatsapp Bot Create By Mr Senesh 🍂✨
 ┏━━━━━━━━━━━━━━━━━━━━━
-┃ *Version*: 1.0.0
-┃*Memory*: 38.09MB/7930MB
-┃ *Runtime*: 1 minute,25 seconds
-┃ *Owner*: Mr Senesh
+┃ Version: 1.0.0
+┃ Memory: 38.09MB/7930MB
+┃ Runtime: 1 minute,25 seconds
+┃ Owner: Mr Senesh
 ┗━━━━━━━━━━━━━━━━━━━━━
 I am hyper md whatsapp bot. How can I help you.
 To get the menu, type as menu. If you need to know something about the bot,
 type as owner and direct the question to me. Good day.
 
- ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ`
+©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ`
 const config = await readEnv();
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
