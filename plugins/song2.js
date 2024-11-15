0aba1ee6384bb76e08946f02acaa9c2ad9988e66
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-15 17:28:17 +0530

    Create song2.js

---
+++ plugins/song2.js
@@ -0,0 +1,50 @@
+
+const { cmd } = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://dark-yasiya-api-new.vercel.app' 
+
+cmd({
+    pattern: "song2",
+    desc: "download songs.",
+    category: "download",
+    react: "🎧",
+    filename: __filename
+},
+async(conn, mek, m,{from, reply, q}) => {
+try{
+
+if(!q) return reply('Give me song name or url !')
+    
+const search = await fetchJson(`${apilink}/search/yt?q=${q}`)
+const data = search.result.data[0];
+const url = data.url
+    
+const ytdl = await fetchJson(`${apilink}/download/ytmp3?url=${data.url}`)
+    
+let message = `‎‎       
+🎶 Hyper-MD YT SONG DOWNLOADER 🎶
+
+
+ ‎Title: ${data.title}
+ Duration: ${data.timestamp}
+ Uploaded: ${data.ago}
+ Views: ${data.views}
+ Author: ${data.author.name}
+Url: ${data.url}
+
+@ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ 
+`
+  
+await conn.sendMessage(from, { image: { url : data.thumbnail }, caption: message }, { quoted : mek })
+  
+// SEND AUDIO NORMAL TYPE and DOCUMENT TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg" }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.result.dl_link }, mimetype: "audio/mpeg", fileName: data.title + ".mp3", caption: `@ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ `}, { quoted: mek })
+  
+} catch(e){
+console.log(e)
+reply(e)
+}
+})
+
