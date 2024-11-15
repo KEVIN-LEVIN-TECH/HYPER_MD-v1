16741da13a34c39da4e63d20d64a703d26e676cd
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-15 16:34:08 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,29 @@
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "owner",
+    desc: "owner the bot",
+    category: "main",
+    react: "👨‍💻",
+    filename: __filename
+},
+
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let dec = `Hyper MD Whatsapp Bot
+
+| ᴏᴡɴᴇʀ ɴᴀᴍᴇ: Mr Senesh (KEVIN)
+| ɴᴜᴍʙᴇʀ: 94784337506
+| ʏᴏᴜᴛᴜʙᴇ: 
+| ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ: 
+
+©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
+`
+await conn.sendMessage(from,{image:{url: `https://i.ibb.co/820JmBT/Colorful-Modern-Infinity-Technology-Free-Logo.png`},caption:dec},{quoted:mek});
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
