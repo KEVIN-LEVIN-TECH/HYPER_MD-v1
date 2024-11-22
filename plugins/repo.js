6c3bde28a73a6b1233fc05bba4f9b0dd8fdd5932
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-22 19:20:53 +0530

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -27,6 +27,11 @@ let dec = `👨‍💻 Hyper MD Repastitory Information
 
 ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
 `
+await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/KEVIN-LEVIN-TECH/Hyper-md-voice/raw/refs/heads/main/auto_voice/ca15f4b2-da73-4901-90ad-6ed40b743bfe.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
 return await conn.sendMessage(from,{image:{url: `https://pomf2.lain.la/f/ozh9ihol.png`},caption:dec},{quoted:mek});
 }catch(e){
 console.log(e)
