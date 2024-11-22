a12c8839badc5ffd9179d600f0fb692b4a7a92bd
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-22 19:20:04 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -61,6 +61,11 @@ ${menu.search}
 
 ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
 `
+await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/KEVIN-LEVIN-TECH/Hyper-md-voice/raw/refs/heads/main/auto_voice/ca15f4b2-da73-4901-90ad-6ed40b743bfe.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
     
 }catch(e){
