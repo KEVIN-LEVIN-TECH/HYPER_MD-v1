89dc4966f588989f3c7870cf9842f2d8e14a7c33
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-11-22 19:20:26 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -20,6 +20,11 @@ let dec = `Hyper MD Whatsapp Bot Owner
 
 ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
 `
+await conn.sendMessage(from, {
+            audio: { url: 'https://github.com/KEVIN-LEVIN-TECH/Hyper-md-voice/raw/refs/heads/main/auto_voice/ca15f4b2-da73-4901-90ad-6ed40b743bfe.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
 await conn.sendMessage(from,{image:{url: `https://pomf2.lain.la/f/ozh9ihol.png`},caption:dec},{quoted:mek});
 
 }catch(e){
