a1949de6e1b4071c46ddbd524b2076f798bb74d5
Omindu Dissanayaka <90664344+OmiduAnjane@users.noreply.github.com>
2024-11-25 18:35:47 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -14,13 +14,26 @@ try{
 let dec = `Hyper MD Whatsapp Bot Owner
 
 | ᴏᴡɴᴇʀ ɴᴀᴍᴇ: Mr Senesh (KEVIN)
-| ɴᴜᴍʙᴇʀ: 94784337506
 | ʏᴏᴜᴛᴜʙᴇ: 
 | ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ: https://whatsapp.com/channel/0029VamA19KFCCoY1q9cvn2I
 
 ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ꜱᴇɴᴇꜱʜ
 `
 await conn.sendMessage(from,{image:{url: `https://i.ibb.co/3cmC926/20241123-124107.jpg` },caption:dec},{quoted:mek});
+    const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN:Mr. Senesh\n'
+            + 'ORG:Mr. Senesh\n'
+            + 'TEL;type=CELL;type=VOICE;waid=94784337506:+94 78 433 7506\n'
+            + 'EMAIL:senesh@gmail.com\n'
+            + 'END:VCARD';
+                        await conn.sendMessage(from, { 
+        contacts: { 
+            displayName: 'Mr. Senesh', 
+            contacts: [{ vcard }] 
+        }
+    }
+);
 
 }catch(e){
 console.log(e)
