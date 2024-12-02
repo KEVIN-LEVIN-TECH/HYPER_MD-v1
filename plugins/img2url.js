d8c8a0fb08f6d0e0767d48b7edd3854150b4658c
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-12-02 07:57:56 +0530

    Update img2url.js

--- plugins/img2url.js
+++ plugins/img2url.js
@@ -1,5 +1,5 @@
 const { cmd, commands } = require('../command');
-let { img2url } = require('@dark-yasiya/imgbb');
+let { img2url } = require('@dark-yasiya/imgbb.js');
 const { getRandom } = require('../lib/functions');
 const fs = require('fs');
 const config = require('../config')
